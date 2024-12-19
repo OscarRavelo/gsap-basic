5c8c1d385ade8a8a2ae85bd3cc7f192963e730e8
Oscar Ravelo <oscar91ravelo@gmail.com>
2024-12-19 05:04:43 +0100

    basic animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
-import * as gsap from "gsap";
+import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 function App() {
+  useGSAP(() => {
+    gsap.to(".box", {
+      xPercent: 100,
+      yPercent: 200,
+      transformOrigin: "center 50%",
+      yoyo: true,
+    });
+  }, []);
   return (
     <div className="bg-black w-full h-lvh text-white justify-center items-center flex  ">
-      <div className="w-32 h-32 bg-green-400"></div>
+      <div className="w-32 h-32 bg-green-400 box"></div>
     </div>
   );
 }
